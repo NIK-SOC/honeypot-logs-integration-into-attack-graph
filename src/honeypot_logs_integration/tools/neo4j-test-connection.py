b9f725c5a026800a7c6ebfbca0681d19a44c008d
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-12-05 20:19:51 +0100

    Create neo4j-test-connection.py

---
+++ src/honeypot_logs_integration/tools/neo4j-test-connection.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+from neo4j import GraphDatabase
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Neo4j connection details
+uri = os.getenv("NEO4J_URI")
+username = os.getenv("NEO4J_USER")
+password = os.getenv("NEO4J_PASSWORD")
+database = os.getenv("NEO4J_DATABASE")
+
+if not uri or not username or not password or not database:
+    raise ValueError("Environment variables are not set properly.")
+
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+def test_connection():
+    try:
+        with driver.session(database=database) as session:
+            greeting = session.run("RETURN 'Connected to the database securely!' AS message").single()
+            print(greeting["message"])
+    except Exception as e:
+        print("Failed to connect to Neo4j:", str(e))
+    finally:
+        driver.close()
+
+test_connection()
